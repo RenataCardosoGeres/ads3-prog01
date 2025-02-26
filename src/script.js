5b4b73fcacb923b32541af42c832029d603928a3
RENATACARDOSOGE\SAMSUNG <renatadante222@gmail.com>
2025-02-26 20:06:28 -0300

    meu primeiro commit

---
+++ src/script.js
@@ -0,0 +1,10 @@
+function calcularidade(){
+    let anoDigitado = document.getElementById("anoNascimento").value;
+
+    let idade = 2025 - anoDigitado;
+
+    console.log(idade);
+
+    document.getElementById("resultado").innerHTML = 
+    `Idade é ${idade}`
+}
